00451c206627933d8d6bde2a54d2565a49e4f492
Patryk Maślanka <p.atryk@Outlook.com>
2026-03-29 12:10:42 +0200

    Increase Android app version to 2.3.0 (#2)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "pl.masslany.podkop"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "2.2.0"
+        versionCode = 13
+        versionName = "2.3.0"
 
         buildConfigField("String", "WYKOP_KEY", "\"${apikeyProperties.getProperty("WYKOP_KEY")}\"")
         buildConfigField("String", "WYKOP_SECRET", "\"${apikeyProperties.getProperty("WYKOP_SECRET")}\"")
